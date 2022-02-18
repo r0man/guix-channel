8b193946f6b3bb3c0709ea4868e5f98fdf5d8046
r0man <roman@burningswell.com>
2022-02-18 14:43:08 +0100

    Add emacs-kubel

--- r0man/packages/emacs.scm
+++ r0man/packages/emacs.scm
@@ -631,6 +631,45 @@ Run the tests with:
 The process must output one JSON message per line.")
     (license #f)))
 
+(define-public emacs-kubel
+  (package
+    (name "emacs-kubel")
+    (version "20220104.2320")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/abrochard/kubel.git")
+             (commit "68d2925c7942039e3fb3eb6c113adec5369c6c72")))
+       (sha256
+        (base32 "173ympabfa14rc6y4f3rjxapj7py0dsnzp8zg7q2gkyxv1iwhh55"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-transient emacs-dash emacs-s emacs-yaml-mode))
+    (arguments
+     '(#:include
+       '("^[^/]+.el$"
+         "^[^/]+.el.in$"
+         "^dir$"
+         "^[^/]+.info$"
+         "^[^/]+.texi$"
+         "^[^/]+.texinfo$"
+         "^doc/dir$"
+         "^doc/[^/]+.info$"
+         "^doc/[^/]+.texi$"
+         "^doc/[^/]+.texinfo$")
+       #:exclude
+       '("^.dir-locals.el$"
+         "^test.el$"
+         "^tests.el$"
+         "^[^/]+-test.el$"
+         "^[^/]+-tests.el$"
+         "^kubel-evil.el$")))
+    (home-page "https://github.com/abrochard/kubel")
+    (synopsis "Control Kubernetes with limited permissions")
+    (description
+     "Emacs extension for controlling Kubernetes with limited permissions.")
+    (license #f)))
+
 (define-public emacs-language-detection
   (package
     (name "emacs-language-detection")
