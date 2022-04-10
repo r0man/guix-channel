c9b4bb44a37e770a4aa4c9860352200294087ab7
r0man <roman@burningswell.com>
2022-04-10 09:59:27 +0200

    Add emacs-wsd-mode

--- r0man/packages/emacs.scm
+++ r0man/packages/emacs.scm
@@ -1266,6 +1266,52 @@ documentation at https://github.com/porterjamesj/virtualenvwrapper.el for more
 details.")
     (license #f)))
 
+(define-public emacs-wsd-mode
+  (package
+    (name "emacs-wsd-mode")
+    (version "20191031.1211")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/josteink/wsd-mode.git")
+             (commit "53330a2a43c4875f8682457df1a869a4c9028660")))
+       (sha256
+        (base32 "1swwaphlbvpalk7mgdgkaal8ffivhxil5fhfkxzazqnkl4i5zbmy"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/josteink/wsd-mode")
+    (synopsis "Emacs major-mode for www.websequencediagrams.com")
+    (description
+     " This is a major-mode for modelling and editing sequence-diagrams using the
+syntax employed by the online service www.websequencediagrams.com.
+
+The mode supports inline rendering the diagrams through the API provided through
+the website and persisting these to image-files next to the files used to
+generate them.
+
+It will automatically activate for files with a WSD-extension.
+
+
+Features:
+
+- syntax higlighting of reccognized keywords - automatic indentation of
+block-statements - generating and saving diagrams generated through WSD's online
+API.  - support for WSD premium features (svg-export, etc) if API-key is
+provided.  - rendering diagrams inline in Emacs, or in external OS viewer if
+image   format is not supported by Emacs.
+
+
+Customization:
+
+To create mode-specific emacs-customizations, please use the wsd-mode-hook.
+
+A short summary of customizable variables:
+
+- wsd-api-key (default blank.  required for premium-features.) - wsd-format
+(default png.  svg requires premium, thus api-key.) - wsd-style (default
+modern-blue) - wsd-indent-offset (default 4) - wsd-font-lock-keywords")
+    (license #f)))
+
 (define-public emacs-x509-mode
   (package
     (name "emacs-x509-mode")
