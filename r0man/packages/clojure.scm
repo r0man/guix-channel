cf6ee96e399d967ac0eccad919746ddfe8d472cf
r0man <roman@burningswell.com>
2022-07-10 11:14:22 +0200

    Add clojure-lsp

---
+++ r0man/packages/clojure.scm
@@ -0,0 +1,30 @@
+(define-module (r0man packages clojure)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system copy)
+  #:use-module (gnu packages compression)
+  #:use-module (guix download)
+  #:use-module (guix packages))
+
+(define-public clojure-lsp
+  (package
+    (name "clojure-lsp")
+    (version "2022.06.29-19.32.13")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/clojure-lsp/clojure-lsp/releases/download/"
+             version
+             "/clojure-lsp-native-static-linux-amd64.zip"))
+       (sha256
+        (base32 "1ckmjh0mfi961vjcqzdk0r5f7kazvc34rmq6gbkxba3kg7rv18yg"))))
+    (build-system copy-build-system)
+    (native-inputs (list unzip))
+    (arguments
+     `(#:install-plan
+       '(("clojure-lsp" "bin/"))))
+    (home-page "https://clojure.org/releases/tools")
+    (synopsis "CLI tools for the Clojure programming language")
+    (description "The Clojure command line tools can be used to start a
+Clojure repl, use Clojure and Java libraries, and start Clojure programs.")
+    (license license:epl1.0)))
