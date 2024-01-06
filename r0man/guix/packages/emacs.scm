fa0065852bc1eb1a5edbaddb132aa9d91ff89c94
Roman Scherer <roman@burningswell.com>
2024-01-06 14:15:05 +0100

    Use copilot commit 653fe7b12990b5b7a050971bed4579022ce4b4f8

--- r0man/guix/packages/emacs.scm
+++ r0man/guix/packages/emacs.scm
@@ -214,7 +214,7 @@ with consult, such as vertico.")
     (license license:gpl3+)))
 
 (define-public emacs-copilot
-  (let ((commit "d4fa14cea818e041b4a536c5052cf6d28c7223d7"))
+  (let ((commit "653fe7b12990b5b7a050971bed4579022ce4b4f8"))
     (package
       (name "emacs-copilot")
       (version (git-version "0.0.1" "2" commit))
@@ -226,7 +226,7 @@ with consult, such as vertico.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "1bn2im5ybzmwbwbi7v39s0qzmca5isp7zb0ls61y6sramh6k8fsg"))))
+          (base32 "1cy3541l33ri4qai61f3l4179q43np9flh5ank5l5a35a1c19gad"))))
       (build-system emacs-build-system)
       (arguments
        (list
