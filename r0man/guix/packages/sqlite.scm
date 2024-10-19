aa390c3960fbed30cc581d6cb2c609bb0119d470
Roman Scherer <roman@burningswell.com>
2024-10-19 11:12:58 +0200

    Set SQLITE_VSS_CMAKE_VERSION

--- r0man/guix/packages/sqlite.scm
+++ r0man/guix/packages/sqlite.scm
@@ -6,6 +6,7 @@
   #:use-module (gnu packages maths)
   #:use-module (gnu packages sqlite)
   #:use-module (guix build-system cmake)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
@@ -37,13 +38,19 @@
                   (("add_subdirectory.*vendor/json.*")
                    "")))))
     (build-system cmake-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'configure 'configure-version
+            (lambda* _
+              (setenv "SQLITE_VSS_CMAKE_VERSION" #$version))))))
     (inputs (list faiss libomp-17 lapack nlohmann-json sqlite))
     (home-page "https://github.com/asg017/sqlite-vss")
     (synopsis "A SQLite extension for efficient vector search, based on Faiss!")
-    (description
-     "SQLite Vector Similarity Search is a SQLite extension
-that brings vector search capabilities to SQLite, based on Faiss.  It
-can be used to build semantic search engines, recommendations, or
+    (description "SQLite Vector Similarity Search is a SQLite extension that brings
+vector search capabilities to SQLite, based on Faiss.  It can be used
+to build semantic search engines, recommendations, or
 questions-and-answering tools.")
     (license license:expat)))
 
