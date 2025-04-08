5335f9d58dfe34641f39c3342d8b4d114d72c8bb
Roman Scherer <roman@burningswell.com>
2025-04-08 10:56:04 +0200

    Add curl-with-openssl

---
+++ r0man/guix/packages/curl.scm
@@ -0,0 +1,18 @@
+(define-module (r0man guix packages curl)
+  #:use-module ((gnu packages curl) #:prefix curl:)
+  #:use-module (gnu packages tls)
+  #:use-module (guix packages)
+  #:use-module (guix utils))
+
+(define-public curl-with-openssl
+  (package
+    (inherit curl:curl)
+    (arguments
+     (substitute-keyword-arguments (package-arguments curl:curl)
+       ((#:configure-flags flags)
+        `(list "--disable-static"
+               "--with-openssl"))))
+    (inputs
+     (modify-inputs (package-inputs curl:curl)
+       (delete "gnutls")
+       (prepend openssl)))))
