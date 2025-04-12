ee7ba706c81fe5674bba763e31f3e71d4d925856
Roman Scherer <roman@burningswell.com>
2025-04-12 14:52:44 +0200

    Add curl-with-libressl

--- r0man/guix/packages/curl.scm
+++ r0man/guix/packages/curl.scm
@@ -4,6 +4,20 @@
   #:use-module (guix packages)
   #:use-module (guix utils))
 
+(define-public curl-with-libressl
+  (package
+    (inherit curl:curl)
+    (name "curl-with-libressl")
+    (arguments
+     (substitute-keyword-arguments (package-arguments curl:curl)
+       ((#:configure-flags flags)
+        `(list "--disable-static"
+               "--with-openssl"))))
+    (inputs
+     (modify-inputs (package-inputs curl:curl)
+       (delete "gnutls")
+       (prepend libressl)))))
+
 (define-public curl-with-openssl
   (package
     (inherit curl:curl)
