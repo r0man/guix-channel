ce193090ceb5742eb69a20cbd262f15b4c57e22c
Roman Scherer <roman@burningswell.com>
2025-10-21 12:28:50 +0200

    gnu: beads: Update to 0.9.11.
    * r0man/guix/packages/golang-apps.scm (beads): Update to 0.9.11.

--- r0man/guix/packages/golang-apps.scm
+++ r0man/guix/packages/golang-apps.scm
@@ -16,27 +16,33 @@
 (define-public beads
   (package
     (name "beads")
-    (version "0.9.10")
+    (version "0.9.11")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/steveyegge/beads")
-             (commit (string-append "v" version))))
+              (url "https://github.com/steveyegge/beads")
+              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "014fcg7kl2bqv0ss569f5rqfrl9zqv1ggxvdb4dlq93qwmxx62fr"))))
+         "1az2gcpk3yksnsq53g0prmhaf22ax1k8bi3lhvi08gzkwncid6x2"))))
     (build-system go-build-system)
     (arguments
      (list
       #:install-source? #f
       #:import-path "github.com/steveyegge/beads/cmd/bd"
-      #:unpack-path "github.com/steveyegge/beads"))
+      #:unpack-path "github.com/steveyegge/beads"
+      #:test-flags
+      #~(list "-skip"
+              (string-join
+               (list "TestGetSocketPath")
+               "|"))))
     (propagated-inputs
      (list go-github-com-anthropics-anthropic-sdk-go
            go-github-com-fatih-color
            go-github-com-spf13-cobra
+           go-gopkg-in-natefinch-lumberjack-v2
            go-modernc-org-sqlite
            go-rsc-io-script))
     (home-page "https://github.com/steveyegge/beads")
