e01ab34de6acb50a36c1b1d057a91fe77e290669
Roman Scherer <roman@burningswell.com>
2025-11-28 14:08:22 +0100

    gnu: node-anthropic-ai-claude-code: Update to 2.0.55.
    * r0man/guix/packages/node.scm (node-anthropic-ai-claude-code): Update to 2.0.55.

--- r0man/guix/packages/node.scm
+++ r0man/guix/packages/node.scm
@@ -8,7 +8,7 @@
 (define-public node-anthropic-ai-claude-code
   (package
     (name "node-anthropic-ai-claude-code")
-    (version "2.0.53")
+    (version "2.0.55")
     (source
      (origin
        (method url-fetch)
@@ -16,7 +16,7 @@
              "https://registry.npmjs.org/@anthropic-ai/claude-code/"
              "-/claude-code-" version ".tgz"))
        (sha256
-        (base32 "0rzby6h1ks83waj3v807drwifv406b64hh0ysx7byz3g3szz0rpk"))))
+        (base32 "14crmr25il0ikqg3971zwg9p36nr5zc4xfynswi70wmbh7g83bsw"))))
     (build-system node-build-system)
     (arguments
      (list
