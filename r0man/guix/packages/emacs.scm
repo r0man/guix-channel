4df8dc40a62bc3c33db111a4e63b73289b111129
furiosa <roman@burningswell.com>
2026-01-04 19:20:14 +0100

    gnu: emacs-scss-mode: Fix lint issues
    Removed .git suffix from URL, added file-name, fixed description formatting.

--- r0man/guix/packages/emacs.scm
+++ r0man/guix/packages/emacs.scm
@@ -1355,27 +1355,29 @@ Org files to JIRA markup for pasting into JIRA tickets & comments.")
       (license license:gpl3+))))
 
 (define-public emacs-scss-mode
-  (package
-    (name "emacs-scss-mode")
-    (version "20180123.1708")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/antonj/scss-mode.git")
-             (commit "cf58dbec5394280503eb5502938f3b5445d1b53d")))
-       (sha256
-        (base32 "0raja19l0igwr0pn0ghr1pj1d8i9k3m3764ma4r8nwzxcj9qw4ja"))))
-    (build-system emacs-build-system)
-    (home-page "https://github.com/antonj/scss-mode")
-    (synopsis "Major mode for editing SCSS files")
-    (description
-     "Command line utility sass is required, see http://sass-lang.com/ To install
-sass: gem install sass
-
-Also make sure sass location is in emacs PATH, example: (setq exec-path (cons
-(expand-file-name \"~/.gem/ruby/1.8/bin\") exec-path)) or customize\n`scss-sass-command' to point to your sass executable.")
-    (license license:gpl3+)))
+  (let ((commit "cf58dbec5394280503eb5502938f3b5445d1b53d")
+        (revision "1"))
+    (package
+      (name "emacs-scss-mode")
+      (version (git-version "0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/antonj/scss-mode")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0raja19l0igwr0pn0ghr1pj1d8i9k3m3764ma4r8nwzxcj9qw4ja"))))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/antonj/scss-mode")
+      (synopsis "Major mode for editing SCSS files")
+      (description
+       "Command line utility sass is required, see @url{http://sass-lang.com/}.
+To install sass: @code{gem install sass}.  Also make sure sass location is
+in Emacs PATH, or customize @code{scss-sass-command} to point to your
+sass executable.")
+      (license license:gpl3+))))
 
 (define-public emacs-smooth-scrolling
   (package
