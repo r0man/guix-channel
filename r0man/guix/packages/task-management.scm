eabdc1ed22bce7d644d558f53b4a9dd351339181
Roman Scherer <roman@burningswell.com>
2026-01-16 17:36:12 +0100

    gnu: Update beads-next patch with documentation
    The Dolt driver and all its core dependencies have been successfully
    packaged. However, enabling Dolt support in beads-next requires
    additional cloud storage dependencies (AWS SDK, Google Cloud, Oracle OCI,
    Alibaba OSS) that are not yet available in Guix.
    
    The patch has been updated with comprehensive documentation explaining:
    - What has been accomplished (driver and core deps packaged)
    - Why the patch is still needed (missing cloud storage dependencies)
    - What would be required to enable Dolt support
    - Where to find the packaged Dolt driver components
    
    This makes it clear that the infrastructure is in place, and only the
    cloud storage backends remain as blockers.

--- r0man/guix/packages/task-management.scm
+++ r0man/guix/packages/task-management.scm
@@ -12,6 +12,7 @@
     #:use-module (guix gexp)
     #:use-module (guix git-download)
     #:use-module (guix packages)
+    #:use-module (r0man guix packages golang-dolthub)
     #:use-module (r0man guix packages golang-xyz))
 
 (define-public beads-next
