0ded34125d342522c97baaa4db73f50ad32e316d
Roman Scherer <roman@burningswell.com>
2026-02-26 18:33:14 +0100

    gnu: beads-next: Update to 0.56.1.

--- r0man/guix/packages/task-management.scm
+++ r0man/guix/packages/task-management.scm
@@ -15,150 +15,140 @@
     #:use-module (guix gexp)
     #:use-module (guix git-download)
     #:use-module (guix packages)
-    #:use-module (r0man guix packages golang-dolthub)
+    #:use-module (r0man guix packages golang-web)
     #:use-module (r0man guix packages golang-xyz))
 
 (define-public beads-next
-  (let ((commit "7d27a34fce13980c61b39baf31518ef4b691632a")
-        (revision "1"))
-    (package
-      (name "beads-next")
-      (version (git-version "0.54.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/steveyegge/beads")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0hh96xad5ppm89igrqirlpf4wc6iyx44xx35pj297r8ybhgm4xvh"))))
-      (build-system go-build-system)
-      (arguments
-       (list
-        #:install-source? #f
-        #:import-path "github.com/steveyegge/beads/cmd/bd"
-        #:unpack-path "github.com/steveyegge/beads"
-        #:phases
-        #~(modify-phases %standard-phases
-            (replace 'check
-              (lambda* (#:key tests? import-path #:allow-other-keys)
-                (when tests?
-                  ;; Run only unit tests, skipping CLI integration tests that fail
-                  ;; in the sandboxed build environment
-                  (with-directory-excursion (string-append "src/" import-path)
-                    ;; Run tests that don't require full environment
-                    (invoke "go"
-                            "test"
-                            "-v"
-                            "-run"
-                            (string-append "^Test(Parse|ValidationResults"
-                                           "|VersionCommand|Truncate"
-                                           "|GitRevParse)")
-                            ".")))))
-            (add-after 'unpack 'delete-broken-test
-              (lambda _
-                (delete-file
-                 "src/github.com/steveyegge/beads/cmd/bd/integrity_content_test.go")))
-            (add-after 'unpack 'fix-embedded-symlinks
-              (lambda _
-                (use-modules (ice-9 ftw))
-                ;; Replace symlinked files with actual copies
-                ;; to work around Go embed limitation with Guix store
-                (define (copy-symlink-targets dir)
-                  (when (file-exists? dir)
-                    (for-each (lambda (file)
-                                (let ((path (string-append dir "/" file)))
-                                  (when (symbolic-link? path)
-                                    (let ((target (readlink path)))
-                                      (delete-file path)
-                                      (copy-file target path)))))
-                              (scandir dir
-                                       (lambda (f)
-                                         (not (member f
-                                                      '("." ".."))))))))
-                ;; Fix sqlite3 WASM files
-                (let ((sqlite-dir "src/github.com/ncruces/go-sqlite3"))
-                  (for-each (lambda (wasm-file)
-                              (when (and (file-exists? wasm-file)
-                                         (symbolic-link? wasm-file))
-                                (let ((target (readlink wasm-file)))
-                                  (delete-file wasm-file)
-                                  (copy-file target wasm-file))))
-                            (list (string-append sqlite-dir
-                                                 "/embed/sqlite3.wasm")
-                                  (string-append sqlite-dir "/util/sql3util"
-                                                 "/wasm/sql3parse_table.wasm"))))
-                ;; Fix chroma lexer and style embedded files
-                (copy-symlink-targets
-                 "src/github.com/alecthomas/chroma/v2/lexers/embedded")
-                (copy-symlink-targets
-                 "src/github.com/alecthomas/chroma/v2/styles")
-                ;; Fix go-mysql-server encoding weight maps
-                (copy-symlink-targets
-                 "src/github.com/dolthub/go-mysql-server/sql/encodings")
-                ;; Fix dolt AGENT.md embedded file
-                (let ((agent-md (string-append "src/github.com/dolthub/dolt"
-                                 "/go/libraries/doltcore/doltdb" "/AGENT.md")))
-                  (when (and (file-exists? agent-md)
-                             (symbolic-link? agent-md))
-                    (let ((target (readlink agent-md)))
-                      (delete-file agent-md)
-                      (copy-file target agent-md))))))
-            (add-before 'build 'set-home
-              (lambda _
-                (setenv "HOME" "/tmp")))
-            (add-after 'install 'install-completions
-              (lambda* (#:key outputs #:allow-other-keys)
-                (let* ((out (assoc-ref outputs "out"))
-                       (bd (string-append out "/bin/bd"))
-                       (bash-dir (string-append out "/etc/bash_completion.d"))
-                       (zsh-dir (string-append out "/share/zsh/site-functions"))
-                       (fish-dir (string-append out
-                                  "/share/fish/vendor_completions.d")))
-                  (mkdir-p bash-dir)
-                  (mkdir-p zsh-dir)
-                  (mkdir-p fish-dir)
-                  (with-output-to-file (string-append bash-dir "/bd")
-                    (lambda ()
-                      (system* bd "completion" "bash")))
-                  (with-output-to-file (string-append zsh-dir "/_bd")
-                    (lambda ()
-                      (system* bd "completion" "zsh")))
-                  (with-output-to-file (string-append fish-dir "/bd.fish")
-                    (lambda ()
-                      (system* bd "completion" "fish")))))))))
-      (native-inputs (list git
-                           icu4c
-                           go-github-com-anthropics-anthropic-sdk-go
-                           go-github-com-burntsushi-toml
-                           go-github-com-cenkalti-backoff-v4
-                           go-github-com-charmbracelet-glamour
-                           go-github-com-charmbracelet-huh
-                           go-github-com-charmbracelet-lipgloss
-                           go-github-com-dolthub-driver
-                           go-github-com-fsnotify-fsnotify
-                           go-github-com-go-sql-driver-mysql
-                           go-github-com-muesli-termenv
-                           go-github-com-ncruces-go-sqlite3
-                           go-github-com-olebedev-when
-                           go-github-com-spf13-cobra
-                           go-github-com-spf13-viper
-                           go-golang-org-x-sys
-                           go-golang-org-x-term
-                           go-gopkg-in-yaml-v3
-                           go-rsc-io-script))
-      (home-page "https://github.com/steveyegge/beads")
-      (synopsis "Graph-based issue tracker for AI coding agents")
-      (description
-       "@command{bd} (Beads) is a lightweight memory system for coding
+  (package
+    (name "beads-next")
+    (version "0.56.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/steveyegge/beads")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0jbbayz0xlk5kaadyaxww2jqkcsdxsav5anka9qirkwja0lsd7w6"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:install-source? #f
+      #:import-path "github.com/steveyegge/beads/cmd/bd"
+      #:unpack-path "github.com/steveyegge/beads"
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'check
+            (lambda* (#:key tests? import-path #:allow-other-keys)
+              (when tests?
+                ;; Run only unit tests, skipping CLI integration tests
+                ;; that fail in the sandboxed build environment.
+                (with-directory-excursion (string-append "src/" import-path)
+                  (invoke "go"
+                          "test"
+                          "-v"
+                          "-run"
+                          (string-append "^Test(Parse|VersionCommand"
+                                         "|Truncate|GitRevParse)")
+                          ".")))))
+          (add-after 'unpack 'fix-embedded-symlinks
+            (lambda _
+              (use-modules (ice-9 ftw))
+              ;; Replace symlinked files with actual copies
+              ;; to work around Go embed limitation with Guix store.
+              (define (copy-symlink-targets dir)
+                (when (file-exists? dir)
+                  (for-each (lambda (file)
+                              (let ((path (string-append dir "/" file)))
+                                (when (symbolic-link? path)
+                                  (let ((target (readlink path)))
+                                    (delete-file path)
+                                    (copy-file target path)))))
+                            (scandir dir
+                                     (lambda (f)
+                                       (not (member f
+                                                    '("." ".."))))))))
+              ;; Fix sqlite3 WASM files.
+              (let ((sqlite-dir "src/github.com/ncruces/go-sqlite3"))
+                (for-each (lambda (wasm-file)
+                            (when (and (file-exists? wasm-file)
+                                       (symbolic-link? wasm-file))
+                              (let ((target (readlink wasm-file)))
+                                (delete-file wasm-file)
+                                (copy-file target wasm-file))))
+                          (list (string-append sqlite-dir
+                                               "/embed/sqlite3.wasm")
+                                (string-append sqlite-dir "/util/sql3util"
+                                               "/wasm/sql3parse_table.wasm"))))
+              ;; Fix chroma lexer and style embedded files.
+              (copy-symlink-targets
+               "src/github.com/alecthomas/chroma/v2/lexers/embedded")
+              (copy-symlink-targets
+               "src/github.com/alecthomas/chroma/v2/styles")))
+          (add-before 'build 'set-home
+            (lambda _
+              (setenv "HOME" "/tmp")))
+          (add-after 'install 'install-completions
+            (lambda* (#:key outputs #:allow-other-keys)
+              (let* ((out (assoc-ref outputs "out"))
+                     (bd (string-append out "/bin/bd"))
+                     (bash-dir (string-append out "/etc/bash_completion.d"))
+                     (zsh-dir (string-append out "/share/zsh/site-functions"))
+                     (fish-dir (string-append out
+                                "/share/fish/vendor_completions.d")))
+                (mkdir-p bash-dir)
+                (mkdir-p zsh-dir)
+                (mkdir-p fish-dir)
+                (with-output-to-file (string-append bash-dir "/bd")
+                  (lambda ()
+                    (system* bd "completion" "bash")))
+                (with-output-to-file (string-append zsh-dir "/_bd")
+                  (lambda ()
+                    (system* bd "completion" "zsh")))
+                (with-output-to-file (string-append fish-dir "/bd.fish")
+                  (lambda ()
+                    (system* bd "completion" "fish")))))))))
+    (native-inputs (list git
+                    icu4c
+                    go-github-com-anthropics-anthropic-sdk-go
+                    go-github-com-burntsushi-toml
+                    go-github-com-cenkalti-backoff-v4
+                    go-github-com-cenkalti-backoff-v5
+                    go-github-com-charmbracelet-glamour
+                    go-github-com-charmbracelet-huh
+                    go-github-com-charmbracelet-lipgloss
+                    go-github-com-fsnotify-fsnotify
+                    go-github-com-go-sql-driver-mysql
+                    go-github-com-muesli-termenv
+                    go-github-com-ncruces-go-sqlite3
+                    go-github-com-olebedev-when
+                    go-github-com-spf13-cobra
+                    go-github-com-spf13-viper
+                    go-go-opentelemetry-io-otel
+                    go-go-opentelemetry-io-otel-exporters-otlp-otlpmetric-otlpmetrichttp
+                    go-go-opentelemetry-io-otel-exporters-stdout-stdoutmetric
+                    go-go-opentelemetry-io-otel-exporters-stdout-stdouttrace
+                    go-go-opentelemetry-io-otel-sdk
+                    go-go-opentelemetry-io-otel-sdk-metric
+                    go-go-opentelemetry-io-proto-otlp
+                    go-golang-org-x-sys
+                    go-golang-org-x-term
+                    go-google-golang-org-grpc
+                    go-google-golang-org-protobuf
+                    go-gopkg-in-yaml-v3
+                    go-rsc-io-script))
+    (home-page "https://github.com/steveyegge/beads")
+    (synopsis "Graph-based issue tracker for AI coding agents")
+    (description
+     "@command{bd} (Beads) is a lightweight memory system for coding
 agents, using a graph-based issue tracker.  Four kinds of dependencies
 work to chain issues together like beads, making them easy for agents
 to follow for long distances and reliably perform complex task streams
 in the right order.  It uses SQLite for fast local operations and
 JSONL files stored in git for distributed synchronization across
 machines.")
-      (license license:expat))))
+    (license license:expat)))
 
 (define-public gastown-next
   (let ((commit "1610d7751b6ce50cd6af1e56dd96eadea91f20c3")
