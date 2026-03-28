d32ec085737bee3be73703286071b4ba6e1c8083
Roman Scherer <roman@burningswell.com>
2026-03-28 14:08:00 +0100

    gnu: gastown-next: Update to 0.12.1-6791.f25cc77

--- r0man/guix/packages/task-management.scm
+++ r0man/guix/packages/task-management.scm
@@ -220,8 +220,8 @@ and utility functions needed to interact with Beads databases.")
       (license license:expat))))
 
 (define-public gastown-next
-  (let ((commit "a0f192c0e5d358285c12b5a6d77ebcd601da3f8d")
-        (revision "6790"))
+  (let ((commit "f25cc773e5f6a845a3e4bb6c14648068e40c8cc8")
+        (revision "6791"))
     (package
       (name "gastown-next")
       (version (git-version "0.12.1" revision commit))
@@ -233,7 +233,7 @@ and utility functions needed to interact with Beads databases.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "17mw213c5wkxl3a7srix6yxxh241v4mqil5apgvw1zj8wrc3sf7i"))))
+          (base32 "02658q5cbn818gcxw9r8k9riychzyzwzkcid2i9nsbkw0s5rqrdf"))))
       (build-system go-build-system)
       (arguments
        (list
