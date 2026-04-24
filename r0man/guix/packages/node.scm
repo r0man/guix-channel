fa2fd8308ccaaebdff99370fe6ec661c63fae2c0
Roman Scherer <roman@burningswell.com>
2026-04-24 18:35:44 +0200

    Revert "gnu: node-anthropic-ai-claude-code: Update to 2.1.119."
    This reverts commit d453a902a7890f39de50dbddb196c994d0d5ef2e.

--- r0man/guix/packages/node.scm
+++ r0man/guix/packages/node.scm
@@ -9,7 +9,7 @@
 (define-public node-anthropic-ai-claude-code
   (package
     (name "node-anthropic-ai-claude-code")
-    (version "2.1.119")
+    (version "2.1.111")
     (source
      (origin
        (method url-fetch)
@@ -17,7 +17,7 @@
              "https://registry.npmjs.org/@anthropic-ai/claude-code/"
              "-/claude-code-" version ".tgz"))
        (sha256
-        (base32 "1wimycampcgb1gfy9zx30zjq37b1ld7vrnbqiawf1vavxhr308bh"))))
+        (base32 "067g0zyypl4l73062hkdbid2yyha3326ydmw4dsr2rd48zjm26nv"))))
     (build-system node-build-system)
     (arguments
      (list
