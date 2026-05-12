7d4ad75733ae910aeb8f0e9100a0be5742846dd7
Roman Scherer <roman@burningswell.com>
2026-05-12 14:55:23 +0200

    gnu: claude-code: Update to 2.1.139.

--- modules/r0man/guix/packages/claude.scm
+++ modules/r0man/guix/packages/claude.scm
@@ -15,32 +15,35 @@
 (define-public claude-code
   (package
     (name "claude-code")
-    (version "2.1.119")
+    (version "2.1.139")
     (source
      (origin
        (method url-fetch)
        (uri (string-append
              "https://github.com/anthropics/claude-code/releases/download/v"
              version "/claude-"
-             (cond ((target-aarch64?) "linux-arm64")
-                   ((target-x86-64?) "linux-x64")
-                   (else "linux-x64"))
-             ".tar.gz"))
-       (sha256
-        (base32
-         (cond ((target-aarch64?)
-                "00w5sbgqcbnqn3h2fc9ma01ywqmjqbrjvwkjjwckz39h7agy8vc6")
+             (cond
+               ((target-aarch64?)
+                "linux-arm64")
                ((target-x86-64?)
-                "1s27dmlhy4bsji8j5ciq70y8d2ll43875vvwjfbkjy37q04f7d8g")
-               (else
-                "1s27dmlhy4bsji8j5ciq70y8d2ll43875vvwjfbkjy37q04f7d8g"))))))
+                "linux-x64")
+               (else "linux-x64")) ".tar.gz"))
+       (sha256
+        (base32 (cond
+                  ((target-aarch64?)
+                   "07b4adrqdxxlrbkasd379n83jczw3zxihazzkq9mprrw88dxshl2")
+                  ((target-x86-64?)
+                   "14gf4p6k11ganwhphfq2y074sn8pl3n8k1dxqskxyy88p60i2h1r")
+                  (else "14gf4p6k11ganwhphfq2y074sn8pl3n8k1dxqskxyy88p60i2h1r"))))))
     (build-system binary-build-system)
     (arguments
      (list
       #:strip-binaries? #f
       #:validate-runpath? #f
-      #:patchelf-plan #~'(("claude"))
-      #:install-plan #~'(("." "lib/claude-code/"))
+      #:patchelf-plan
+      #~'(("claude"))
+      #:install-plan
+      #~'(("." "lib/claude-code/"))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'chmod-binary
