745680749b7c85dd20563e270f810ac42c936135
Roman Scherer <roman@burningswell.com>
2026-09-21 16:37:55 +0200

    gnu: dolt: Update to 2.3.5.
    Also update the go-mysql-server-for-dolt and vitess-for-dolt pins that
    the standalone CLI requires (dolt 2.3.5 needs sql.IndexColumnOrder from
    go-mysql-server 0.20.1-0.20260915).

--- modules/r0man/guix/packages/golang-dolthub.scm
+++ modules/r0man/guix/packages/golang-dolthub.scm
@@ -946,34 +946,34 @@ AI assistants to interact with Dolt version-controlled SQL databases.")
   (package
     (inherit go-github-com-dolthub-vitess)
     (name "go-github-com-dolthub-vitess-for-dolt")
-    (version "0.0.0-20260901210329-5364bf844382")
+    (version "0.0.0-20260914165732-909aa98f55b7")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/dolthub/vitess")
-             (commit "5364bf844382521bd76e410cdf90ff563ce0336b")))
+             (commit "909aa98f55b72751f16d2b0de72fe300d17bbbc4")))
        (file-name (git-file-name "go-github-com-dolthub-vitess"
-                                 "0.0.0-20260901210329-5364bf844382"))
+                                 "0.0.0-20260914165732-909aa98f55b7"))
        (sha256
-        (base32 "0psfwiz43zrdg4bwrd7ydc5mah2rdaqcq8jhlqmfdh6i1aidj6nh"))))))
+        (base32 "0wcppsr9wbbk0qapij4hzwj37q7qpvvp0hl7ailv832zax5yx5bs"))))))
 
 
 (define-public go-github-com-dolthub-go-mysql-server-for-dolt
   (package
     (inherit go-github-com-dolthub-go-mysql-server)
     (name "go-github-com-dolthub-go-mysql-server-for-dolt")
-    (version "0.20.1-0.20260901230631-da4d8ec733de")
+    (version "0.20.1-0.20260915233322-a939809e084d")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/dolthub/go-mysql-server")
-             (commit "da4d8ec733de125e923e676416c2573b63c8204c")))
+             (commit "a939809e084d3d67214c53b5d2830771e34e7279")))
        (file-name (git-file-name "go-github-com-dolthub-go-mysql-server"
-                                 "0.20.1-0.20260901230631-da4d8ec733de"))
+                                 "0.20.1-0.20260915233322-a939809e084d"))
        (sha256
-        (base32 "1ac6iapmybphn2dq9ha6s4dc8q320jn2cwm1jmshaykw80jpclsn"))))
+        (base32 "1kg3hvphk2z3av8dwzcragkqjpbljybyhqsi0vcv6pjhd0ckbyk4"))))
     (propagated-inputs
      (modify-inputs (package-propagated-inputs
                      go-github-com-dolthub-go-mysql-server)
@@ -987,7 +987,7 @@ AI assistants to interact with Dolt version-controlled SQL databases.")
     (name "dolt")
     ;; Decoupled from go-github-com-dolthub-dolt-go (pinned at 1.88.1 for
     ;; beads): the standalone CLI tracks the dolt v2 release line.
-    (version "2.3.2")
+    (version "2.3.5")
     (source
      (origin
        (method git-fetch)
@@ -996,7 +996,7 @@ AI assistants to interact with Dolt version-controlled SQL databases.")
              (commit (string-append "v" version))))
        (file-name (git-file-name "dolt" version))
        (sha256
-        (base32 "08w748ddsyqwmsy4k515lpgqfji43y3hcpz2brp32s0qdm3ayqgs"))
+        (base32 "1798a0p7prm6934a0yqd43pb4mbfmn7adny5gyx0c5vxvkyqnis2"))
        (modules '((guix build utils)))
        (snippet
         ;; Replace google.golang.org/grpc/experimental/credentials
